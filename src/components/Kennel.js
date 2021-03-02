a5b886c2cff537043856f45d53900e60e73d7fd1
powellmad <madsnpowell@gmail.com>
2021-03-02 09:27:36 -0600

    added router to index and updated kennel to include the nav bar

--- src/components/Kennel.js
+++ src/components/Kennel.js
@@ -1,4 +1,6 @@
 import React from "react"
+import { NavBar } from "./nav/NavBar"
+import { ApplicationViews } from "./ApplicationViews"
 import "./Kennel.css"
 import { AnimalCard } from "./animals/AnimalCard.js"
 import { CustomerCard } from "./customers/CustomerCard.js"
@@ -7,55 +9,50 @@ import { LocationCard } from "./locations/LocationCard.js"
 import { PropsAndState } from "./PropsAndState.js"
 
 
-export const Kennel = () => {
-    const kennel = {
-        locations: [
-            {
-                city: "Nashville ",
-                area: "North "
-            }
-        ]
-    }
-
-    return ( 
+export const Kennel = () => (
     <>
-        <PropsAndState yourName="Mad" />
-        <h2>Nashville Kennels</h2>
-        <small>Loving care when you're not there.</small>
-        <address>
-            <div>Visit Us at the {kennel.locations[0].city}{kennel.locations[0].area} Location</div>
-            <div>500 Puppy Way</div>
-        </address>
-
-        <h2>Animals</h2>
-        <article className="animals">
-            <AnimalCard />
-            <AnimalCard />
-            <AnimalCard />
-            <AnimalCard />
-            <AnimalCard />
-        </article>
-
-        <h2>Employees</h2>
-        <article className="empolyees">
-            <EmployeeCard />
-            <EmployeeCard />
-            <EmployeeCard />
-            
-        </article>
-
-        <h2>Locations</h2>
-        <article className="locations">
-            <LocationCard />
-            <LocationCard />
-        </article>
-        
-        <h2>Customers</h2>
-        <article className="customers">
-            <CustomerCard />
-            <CustomerCard />
-            <CustomerCard />
-        </article>
+        <NavBar />
+        <ApplicationViews />
     </>
-    )
-}
+)
+
+
+    // <>
+    //     <PropsAndState yourName="Mad" />
+    //     <h2>Nashville Kennels</h2>
+    //     <small>Loving care when you're not there.</small>
+    //     <address>
+    //         <div>Visit Us at the {kennel.locations[0].city}{kennel.locations[0].area} Location</div>
+    //         <div>500 Puppy Way</div>
+    //     </address>
+
+    //     <h2>Animals</h2>
+    //     <article className="animals">
+    //         <AnimalCard />
+    //         <AnimalCard />
+    //         <AnimalCard />
+    //         <AnimalCard />
+    //         <AnimalCard />
+    //     </article>
+
+    //     <h2>Employees</h2>
+    //     <article className="empolyees">
+    //         <EmployeeCard />
+    //         <EmployeeCard />
+    //         <EmployeeCard />
+            
+    //     </article>
+
+    //     <h2>Locations</h2>
+    //     <article className="locations">
+    //         <LocationCard />
+    //         <LocationCard />
+    //     </article>
+        
+    //     <h2>Customers</h2>
+    //     <article className="customers">
+    //         <CustomerCard />
+    //         <CustomerCard />
+    //         <CustomerCard />
+    //     </article>
+    // </>
